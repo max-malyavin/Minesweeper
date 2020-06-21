dede4e0317a4cab9545608765b83c6fb3afcac4f
max-malyavin <max_malyavin@mail.ru>
2020-06-21 11:42:20 +0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="header">
+    header
+      </div>
+      <div className="body">
+    body
+      </div>
+    </div>
+  );
+}
+
+export default App;
